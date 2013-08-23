66135c32c68e9fdf5fe7ef83ab0d441f8622680c
Jim Gapinski <jim@focaltg.com>
2013-08-22 20:12:44 -0500

    Created a railsa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jimbook::Application.config.secret_token = 'b67abec23dbb35828d1722fed2f42feb2daa608d7f27da0227811397b92cece0edc096708fefd155f133d8f8c9ddcf2079cc0e3ddbe2ac8d27783664d05a303c'
